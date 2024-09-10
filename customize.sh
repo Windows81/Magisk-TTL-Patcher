a358d87799b7283c1340fcc7e70184f2b2a5ebbc
Windows81 <nastyahmede@gmail.com>
2024-09-10 08:52:42 +0000

    thl_alphatest10.zip

--- customize.sh
+++ customize.sh
@@ -10,22 +10,21 @@ ui_print "- Codename: $(getprop ro.build.product)"
 ui_print "- Device: $(getprop ro.build.display.id)"
 ui_print "- Fingerprint: $(getprop ro.build.fingerprint)"
 
-# import Magisk util_functions
-. /data/adb/magisk/util_functions.sh
-
-# detect boot slot
-SLOT=$(grep_cmdline androidboot.slot_suffix)
-if [ -z $SLOT ]; then
-    SLOT=$(grep_cmdline androidboot.slot)
-    [ -z $SLOT ] || SLOT=_${SLOT}
+# KernelSU already has magiskboot in PATH
+if [ ! -z "$MAGISKBIN" ]; then
+  # Add Magisk binaries folder to PATH
+  PATH="$MAGISKBIN:$PATH"
+elif [ "$APATCH" == "true" ]; then
+  # Add symlink to magiskboot to APatch binaries folder
+  apk=$(pm path me.bmax.apatch)
+  ln -sf "${apk:8:${#apk}-16}lib/arm64/libmagiskboot.so" /data/adb/ap/bin/magiskboot
 fi
-[ "$SLOT" = "normal" ] && unset SLOT
-[ -z $SLOT ] || ui_print "- Current boot slot: $SLOT"
 
-# set Magisk varibles
-RECOVERYMODE=false
+############################################
+# Magisk General Utility Functions
+############################################
 
-# mirror func without init_boot
+# redefine function excluding init_boot
 find_kern_boot_image() {
   BOOTIMAGE=
   if [ ! -z $SLOT ]; then
@@ -39,106 +38,143 @@ find_kern_boot_image() {
   fi
 }
 
+# missing from KernelSU/APatch
+flash_image() {
+  local CMD1
+  case "$1" in
+    *.gz) CMD1="gzip -d < '$1' 2>/dev/null";;
+    *)    CMD1="cat '$1'";;
+  esac
+  if [ -b "$2" ]; then
+    local img_sz=$(stat -c '%s' "$1")
+    local blk_sz=$(blockdev --getsize64 "$2")
+    [ "$img_sz" -gt "$blk_sz" ] && return 1
+    blockdev --setrw "$2"
+    local blk_ro=$(blockdev --getro "$2")
+    [ "$blk_ro" -eq 1 ] && return 2
+    eval "$CMD1" | cat - /dev/zero > "$2" 2>/dev/null
+  elif [ -c "$2" ]; then
+    flash_eraseall "$2" >&2
+    eval "$CMD1" | nandwrite -p "$2" - >&2
+  else
+    ui_print "- Not block or char device, storing image"
+    eval "$CMD1" > "$2" 2>/dev/null
+  fi
+  return 0
+}
+
+############################################
+
 # find boot partition
 find_kern_boot_image
 [ -z $BOOTIMAGE ] && abort "! Unable to detect boot partition"
 ui_print "- Target image: $BOOTIMAGE"
 
-# change to working directory
-cd $MODPATH
+# create and change to working directory outside of module folder
+mkdir /data/adb/hoppatch
+cd /data/adb/hoppatch
 
 # dump boot image
 if [ -c "$BOOTIMAGE" ]; then
-    nanddump -f boot.img "$BOOTIMAGE"
+  nanddump -f boot.img "$BOOTIMAGE"
 else
-    dd if="$BOOTIMAGE" of=boot.img
+  dd if="$BOOTIMAGE" of=boot.img
 fi
 
 # unpack boot image
 ui_print "- Unpacking boot image with magiskboot"
-chmod +x /data/adb/magisk/magiskboot
-/data/adb/magisk/magiskboot unpack boot.img
+magiskboot unpack boot.img
 [ $? -ne 0 ] && abort "! Failed to unpack boot image"
 sleep 1
 
 # patch with patcher
 ui_print "- Patching boot image..."
 mkdir dalvik-cache
-ANDROID_DATA=$PWD dalvikvm -cp kernel_patcher.zip kernel_patcher
+ANDROID_DATA=$PWD dalvikvm -cp $MODPATH/kernel_patcher.zip kernel_patcher
 [ $? -ne 0 ] && abort "! Kernel patcher failed"
 sleep 1
 
 # repack patched boot image
 ui_print "- Repacking boot image..."
 rm kernel && mv kernel.patched kernel
-/data/adb/magisk/magiskboot repack boot.img
+magiskboot repack boot.img
 [ $? -ne 0 ] && abort "! Failed to repack boot image"
 sleep 1
 
+# change to module directory
+cd $MODPATH
+
 # offload.o was added since Android 11
 if [ $API -ge 30 ]; then
-    # patching offload.o and appending bind commands to post-fs-data.sh
-    ui_print "- Patching offload.o..."
-    cp post-fs-data.sh.template post-fs-data.sh
-    offloadpath=/system/etc/bpf/
+  # patching offload.o and appending bind commands to post-fs-data.sh
+  ui_print "- Patching offload.o..."
+  cp post-fs-data.sh.template post-fs-data.sh
+  offloadpath=/system/etc/bpf/
+  mkdir -p $MODPATH$offloadpath
+  find $offloadpath -maxdepth 1 -type f -iname "offload*.o" | while read offloadfile; do
+    ui_print "- Found BPF module: $offloadfile"
+    cp $offloadfile $MODPATH$offloadpath
+    ANDROID_DATA=$PWD dalvikvm -cp bpf_patcher.zip bpf_patcher $MODPATH$offloadfile $API
+    [ $? -ne 0 ] && abort "! BPF patcher failed"
+    echo "mount -o ro,bind \$MODDIR$offloadfile.patched $offloadfile" >> post-fs-data.sh
+    touch flag
+    sleep 1
+  done
+  find /apex/com.android.tethering*/etc/bpf/ -maxdepth 0 -type d | while read offloadpath; do
     mkdir -p $MODPATH$offloadpath
     find $offloadpath -maxdepth 1 -type f -iname "offload*.o" | while read offloadfile; do
-        ui_print "- Found BPF module: $offloadfile"
-        cp $offloadfile $MODPATH$offloadpath
-        ANDROID_DATA=$PWD dalvikvm -cp bpf_patcher.zip bpf_patcher $MODPATH$offloadfile $API
-        [ $? -ne 0 ] && abort "! BPF patcher failed"
-        echo "mount -o ro,bind \$MODDIR$offloadfile.patched $offloadfile" >> post-fs-data.sh
-        touch flag
-        sleep 1
+      ui_print "- Found BPF module: $offloadfile"
+      cp $offloadfile $MODPATH$offloadpath
+      ANDROID_DATA=$PWD dalvikvm -cp bpf_patcher.zip bpf_patcher $MODPATH$offloadfile $API
+      [ $? -ne 0 ] && abort "! BPF patcher failed"
+      echo "mount -o ro,bind \$MODDIR$offloadfile.patched $offloadfile" >> post-fs-data.sh
+      touch flag
+      sleep 1
     done
-    find /apex/com.android.tethering*/etc/bpf/ -maxdepth 0 -type d | while read offloadpath; do
-        mkdir -p $MODPATH$offloadpath
-        find $offloadpath -maxdepth 1 -type f -iname "offload*.o" | while read offloadfile; do
-            ui_print "- Found BPF module: $offloadfile"
-            cp $offloadfile $MODPATH$offloadpath
-            ANDROID_DATA=$PWD dalvikvm -cp bpf_patcher.zip bpf_patcher $MODPATH$offloadfile $API
-            [ $? -ne 0 ] && abort "! BPF patcher failed"
-            echo "mount -o ro,bind \$MODDIR$offloadfile.patched $offloadfile" >> post-fs-data.sh
-            touch flag
-            sleep 1
-        done
-    done
-    [ ! -f flag ] && abort "! Unable to locate BPF module"
+  done
+  [ ! -f flag ] && abort "! Unable to locate BPF module"
 fi
 
 # disabling 'Tethering hardware acceleration'
 if [ ! $(settings get global tether_offload_disabled) == "1" ]; then
-    ui_print "- Disabling 'Tethering hardware acceleration'..."
-    settings put global tether_offload_disabled 1
+  ui_print "- Disabling 'Tethering hardware acceleration'..."
+  settings put global tether_offload_disabled 1
 else
-    ui_print "- 'Tethering hardware acceleration' already disabled"
+  ui_print "- 'Tethering hardware acceleration' already disabled"
 fi
 
 # adding dun to APN Type
 apninfo=$(content query --uri content://telephony/carriers/preferapn --projection _id:type)
 echo $apninfo | grep "_id" >/dev/null
-[ $? -ne 0 ] && abort "! Failed to dump APN information"
-apnid=$(echo $apninfo | cut -F2 -d"_id=" | cut -F1 -d",")
-apntype=$(echo $apninfo | cut -F2 -d"type=")
-echo $apntype | grep "dun" >/dev/null
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+  apnid=$(echo $apninfo | cut -F2 -d"_id=" | cut -F1 -d",")
+  apntype=$(echo $apninfo | cut -F2 -d"type=")
+  echo $apntype | grep "dun" >/dev/null
+  if [ $? -ne 0 ]; then
     ui_print "- Inserting 'dun' into APN Type"
-    if [ "${str: -1}" -eq "," ]; then
-        content update --uri content://telephony/carriers --where "_id=$apnid" --bind type:s:"$apntype"dun
+    if [ "${apntype: -1}" == "," ]; then
+      content update --uri content://telephony/carriers --where "_id=$apnid" --bind type:s:"$apntype"dun
     else
-        content update --uri content://telephony/carriers --where "_id=$apnid" --bind type:s:"$apntype",dun
+      content update --uri content://telephony/carriers --where "_id=$apnid" --bind type:s:"$apntype",dun
     fi
     # revert edited=4 that prevents APN from being edited from GUI
     apninfo=$(content query --uri content://telephony/carriers/preferapn --projection edited)
     echo $apninfo | grep "edited" >/dev/null
     if [ $? -eq 0 ]; then
-        content update --uri content://telephony/carriers --where "_id=$apnid" --bind edited:s:1
+      content update --uri content://telephony/carriers --where "_id=$apnid" --bind edited:s:1
     fi
-else
+  else
     ui_print "- APN Type already contains a 'dun' entry"
+  fi
+else
+  if [ -f /sdcard/skip_apn ]; then
+    ui_print "! WARNING Failed to dump APN information"
+  else
+    abort "! Failed to dump APN information"
+  fi
 fi
-
 # flash new-boot.img
 ui_print "- Flashing modified boot image..."
-flash_image new-boot.img "$BOOTIMAGE"
+flash_image /data/adb/hoppatch/new-boot.img "$BOOTIMAGE"
 [ $? -ne 0 ] && abort "! Failed to flash modified boot image"
+mv /data/adb/hoppatch/boot.img /data/adb/hoppatch/original-boot.img
--- uninstall.sh
+++ uninstall.sh
@@ -1,23 +1,112 @@
 MODDIR=${0%/*}
 
-# import Magisk util_functions
-. /data/adb/magisk/util_functions.sh
+############################################
+# Magisk General Utility Functions
+############################################
 
-# detect boot slot
-SLOT=$(grep_cmdline androidboot.slot_suffix)
-if [ -z $SLOT ]; then
+toupper() {
+  echo "$@" | tr '[:lower:]' '[:upper:]'
+}
+
+grep_cmdline() {
+  local REGEX="s/^$1=//p"
+  { echo $(cat /proc/cmdline)$(sed -e 's/[^"]//g' -e 's/""//g' /proc/cmdline) | xargs -n 1; \
+    sed -e 's/ = /=/g' -e 's/, /,/g' -e 's/"//g' /proc/bootconfig; \
+  } 2>/dev/null | sed -n "$REGEX"
+}
+
+grep_prop() {
+  local REGEX="s/^$1=//p"
+  shift
+  local FILES=$@
+  [ -z "$FILES" ] && FILES='/system/build.prop'
+  cat $FILES 2>/dev/null | dos2unix | sed -n "$REGEX" | head -n 1
+}
+
+# find_block [partname...]
+find_block() {
+  local BLOCK DEV DEVICE DEVNAME PARTNAME UEVENT
+  for BLOCK in "$@"; do
+    DEVICE=$(find /dev/block \( -type b -o -type c -o -type l \) -iname $BLOCK | head -n 1) 2>/dev/null
+    if [ ! -z $DEVICE ]; then
+      readlink -f $DEVICE
+      return 0
+    fi
+  done
+  # Fallback by parsing sysfs uevents
+  for UEVENT in /sys/dev/block/*/uevent; do
+    DEVNAME=$(grep_prop DEVNAME $UEVENT)
+    PARTNAME=$(grep_prop PARTNAME $UEVENT)
+    for BLOCK in "$@"; do
+      if [ "$(toupper $BLOCK)" = "$(toupper $PARTNAME)" ]; then
+        echo /dev/block/$DEVNAME
+        return 0
+      fi
+    done
+  done
+  # Look just in /dev in case we're dealing with MTD/NAND without /dev/block devices/links
+  for DEV in "$@"; do
+    DEVICE=$(find /dev \( -type b -o -type c -o -type l \) -maxdepth 1 -iname $DEV | head -n 1) 2>/dev/null
+    if [ ! -z $DEVICE ]; then
+      readlink -f $DEVICE
+      return 0
+    fi
+  done
+  return 1
+}
+
+# redefine function excluding init_boot
+find_kern_boot_image() {
+  SLOT=$(grep_cmdline androidboot.slot_suffix)
+  if [ -z $SLOT ]; then
     SLOT=$(grep_cmdline androidboot.slot)
     [ -z $SLOT ] || SLOT=_${SLOT}
-fi
-[ "$SLOT" = "normal" ] && unset SLOT
+  fi
+  [ "$SLOT" = "normal" ] && unset SLOT
+  BOOTIMAGE=
+  if [ ! -z $SLOT ]; then
+    BOOTIMAGE=$(find_block "ramdisk$SLOT" "recovery_ramdisk$SLOT" "boot$SLOT")
+  else
+    BOOTIMAGE=$(find_block ramdisk recovery_ramdisk kern-a android_boot kernel bootimg boot lnx boot_a)
+  fi
+  if [ -z $BOOTIMAGE ]; then
+    # Lets see what fstabs tells me
+    BOOTIMAGE=$(grep -v '#' /etc/*fstab* | grep -E '/boot(img)?[^a-zA-Z]' | grep -oE '/dev/[a-zA-Z0-9_./-]*' | head -n 1)
+  fi
+}
 
-# not uninstalling through recovery mode
-RECOVERYMODE=false
+flash_image() {
+  local CMD1
+  case "$1" in
+    *.gz) CMD1="gzip -d < '$1' 2>/dev/null";;
+    *)    CMD1="cat '$1'";;
+  esac
+  if [ -b "$2" ]; then
+    local img_sz=$(stat -c '%s' "$1")
+    local blk_sz=$(blockdev --getsize64 "$2")
+    [ "$img_sz" -gt "$blk_sz" ] && return 1
+    blockdev --setrw "$2"
+    local blk_ro=$(blockdev --getro "$2")
+    [ "$blk_ro" -eq 1 ] && return 2
+    eval "$CMD1" | cat - /dev/zero > "$2" 2>/dev/null
+  elif [ -c "$2" ]; then
+    flash_eraseall "$2" >&2
+    eval "$CMD1" | nandwrite -p "$2" - >&2
+  else
+    #ui_print "- Not block or char device, storing image"
+    eval "$CMD1" > "$2" 2>/dev/null
+  fi
+  return 0
+}
+
+############################################
 
 # use util_functions to find the boot partition
-find_boot_image
+find_kern_boot_image
 
-if [ ! -z $BOOTIMAGE ] && [ -f $MODDIR/boot.img ]; then
+if [ ! -z $BOOTIMAGE ] && [ -f /data/adb/hoppatch/original-boot.img ]; then
     # Flash original boot image
-    flash_image $MODDIR/boot.img "$BOOTIMAGE"
+    flash_image /data/adb/hoppatch/original-boot.img "$BOOTIMAGE"
 fi
+
+rm -R /data/adb/hoppatch
